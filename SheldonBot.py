cb49f0511889cfe18ba4a0643dbfc6140ef6961e
daedalus4096 <michael.a.bunting@gmail.com>
2021-10-14 22:06:06 -0700

    Add subcommands and message listening

--- SheldonBot.py
+++ SheldonBot.py
@@ -1,5 +1,6 @@
 # SheldonBot.py
 import os
+import sys
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -11,8 +12,24 @@ bot = commands.Bot(command_prefix='!')
 
 
 @bot.command(name='sheldon', help='Ask Sheldon to do something')
-async def sheldon_main(ctx):
-    await ctx.send('Mrow!')
+async def sheldon_main(ctx, cmd):
+    match cmd.lower():
+        case 'speak':
+            await ctx.send('Mrow!')
+        case 'sleep':
+            await ctx.send('Zzz...')
+            sys.exit()
+        case _:
+            await ctx.send('Sheldon looks at you in confusion')
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if 'i love sheldon' in message.content.lower():
+        await message.channel.send('Mrow! :heart:')
 
 
 bot.run(TOKEN)
