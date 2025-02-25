96797942023024ee0977e79db5bb50b39a432069
Michael Bunting <michael.a.bunting@gmail.com>
2025-02-24 21:48:30 -0800

    Create lambda for random image selection

---
+++ api/lambda/GetRandomImage.py
@@ -0,0 +1,30 @@
+import json
+import random
+import boto3
+
+BUCKET = 'sheldonbot-images-21985152'
+CDN_PREFIX = 'https://cdn.sheldonbot.com/'
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    print(event)
+    tag = event['tag']
+
+    # List all objects in the bucket with the requested prefix except for the bucket itself
+    resp = s3.list_objects_v2(Bucket=BUCKET, Prefix=tag+'/', StartAfter=tag+'/')
+
+    # Select a random object from the listing
+    obj = random.choice(resp['Contents']) if 'Contents' in resp else None
+
+    # Return a response
+    if obj is None:
+        return {
+            'statusCode': 404,
+            'body': CDN_PREFIX + 'not_found.png'
+        }
+    else:
+        return {
+            'statusCode': 200,
+            'body': CDN_PREFIX + obj['Key']
+        }
